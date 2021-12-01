25119fbbfc8f543313707aa5202e91ddf3027eca
yoonseulgi <89211245+yoonseulgi@users.noreply.github.com>
2021-12-01 17:07:00 +0900

    Update setup.py
    dd

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
+from os import path
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+install_requires = [
+    'PyYAML', 'SQLAlchemy', 'numpy', 'pandas', 'plotly'
+]
 setup(
-    name="my_package",
-    version="0.1.0",
+    name="createcustomerinfo",
+    version="0.0.1",
+    author="seulgiyoon",
+    description="create customer data in local database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    py_modules=['customer'],
+    url="https://github.com/yoonseulgi/customerpackage.git",
     packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=requirements,
+    python_requires='>=3.8',
+    install_requires=install_requires
 )
